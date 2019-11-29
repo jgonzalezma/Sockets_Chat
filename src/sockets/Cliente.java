10d82344de10fd4362d35e38b7f6fc02d4aeecfd
jgonzalezma <josu0805@gmail.com>
2019-11-29 23:09:52 +0100

    Cliente envia mensaje a servidor con object datos, genera errores ahora

--- src/sockets/Cliente.java
+++ src/sockets/Cliente.java
@@ -4,19 +4,25 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
-import java.awt.BorderLayout;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
-import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class Cliente {
 
 	private JFrame frame;
-	private JTextField textField;
+	private JTextField txtField_mensaje;
+	private JTextArea textArea_mensajes;
+	private JTextField txtField_nick;
+	private JTextField txtField_destino;
 
 	/**
 	 * Launch the application.
@@ -50,31 +56,92 @@ public class Cliente {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		textField = new JTextField();
-		textField.setBounds(86, 30, 258, 20);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		txtField_mensaje = new JTextField();
+		txtField_mensaje.setBounds(86, 48, 258, 20);
+		frame.getContentPane().add(txtField_mensaje);
+		txtField_mensaje.setColumns(10);
 		
 		JButton btnEnviar = new JButton("Enviar");
 		btnEnviar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
 					Socket socket = new Socket("192.168.1.37", 9999);
+					PaqueteEnvio datos = new PaqueteEnvio();
 					
-					DataOutputStream flujo_salida = new DataOutputStream(socket.getOutputStream());
+					datos.setNick(txtField_nick.getText());
+					datos.setIp(txtField_destino.getText());
+					datos.setMensaje(txtField_mensaje.getText());
 					
-					flujo_salida.writeUTF(textField.getText());
-					flujo_salida.close();
-				} catch (UnknownHostException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+					ObjectOutputStream paquete_datos = new ObjectOutputStream(socket.getOutputStream());
+					paquete_datos.writeObject(datos);
+					socket.close();
+					
+					/* DataOutputStream flujo_salida = new DataOutputStream(socket.getOutputStream());
+					
+					flujo_salida.writeUTF(txtField_mensaje.getText());
+					flujo_salida.close(); */
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
 		});
-		btnEnviar.setBounds(163, 61, 89, 23);
+		btnEnviar.setBounds(175, 83, 89, 23);
 		frame.getContentPane().add(btnEnviar);
+		
+		textArea_mensajes = new JTextArea();
+		textArea_mensajes.setBounds(86, 117, 258, 133);
+		frame.getContentPane().add(textArea_mensajes);
+		
+		JLabel lblChat = new JLabel("Chat");
+		lblChat.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblChat.setBounds(190, 11, 67, 14);
+		frame.getContentPane().add(lblChat);
+		
+		txtField_nick = new JTextField();
+		txtField_nick.setBounds(68, 10, 86, 20);
+		frame.getContentPane().add(txtField_nick);
+		txtField_nick.setColumns(10);
+		
+		JLabel lblNick = new JLabel("Nick:");
+		lblNick.setBounds(10, 13, 46, 14);
+		frame.getContentPane().add(lblNick);
+		
+		txtField_destino = new JTextField();
+		txtField_destino.setBounds(338, 10, 86, 20);
+		frame.getContentPane().add(txtField_destino);
+		txtField_destino.setColumns(10);
+		
+		JLabel lblIpDestino = new JLabel("IP destino:");
+		lblIpDestino.setBounds(258, 11, 86, 14);
+		frame.getContentPane().add(lblIpDestino);
+	}
+	
+	class PaqueteEnvio implements Serializable{
+		private String nick, ip, mensaje;
+
+		public String getNick() {
+			return nick;
+		}
+
+		public void setNick(String nick) {
+			this.nick = nick;
+		}
+
+		public String getIp() {
+			return ip;
+		}
+
+		public void setIp(String ip) {
+			this.ip = ip;
+		}
+
+		public String getMensaje() {
+			return mensaje;
+		}
+
+		public void setMensaje(String mensaje) {
+			this.mensaje = mensaje;
+		}
 	}
 }
--- src/sockets/Servidor.java
+++ src/sockets/Servidor.java
@@ -1,14 +1,16 @@
 package sockets;
 
 import java.awt.EventQueue;
-import java.awt.TextArea;
-import java.io.DataInputStream;
+
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
+
+import sockets.Cliente.PaqueteEnvio;
 
 public class Servidor implements Runnable{
 
@@ -57,17 +59,30 @@ public class Servidor implements Runnable{
 
 	@Override
 	public void run() {
-		try {
-			ServerSocket servidor = new ServerSocket(9999);
-			while(true) {
-				Socket socket = servidor.accept();
-				DataInputStream flujo_entrada = new DataInputStream(socket.getInputStream());
-				String mensaje_texto = flujo_entrada.readUTF();
-				textArea.append("\n" + mensaje_texto);
-				socket.close();
-			}		
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+			ServerSocket servidor;
+			String nick, ip, mensaje;
+			PaqueteEnvio paquete_recibido;
+			try {
+				servidor = new ServerSocket(9999);
+				while(true) {
+					Socket socket = servidor.accept();
+					
+					ObjectInputStream paquete_datos = new ObjectInputStream(socket.getInputStream());
+					paquete_recibido = (PaqueteEnvio) paquete_datos.readObject();
+					nick = paquete_recibido.getNick();
+					ip = paquete_recibido.getIp();
+					mensaje = paquete_recibido.getMensaje();
+					
+					textArea.append("\n" + nick + ": " + mensaje + " para " + ip);
+					socket.close();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}	
 }
